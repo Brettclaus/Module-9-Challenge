367db8e4cc35f4f45b0f99d74006c08713c7b7fa
Brettclaus <120690858+Brettclaus@users.noreply.github.com>
2023-11-16 15:02:10 -0800

    Add files via upload

--- Module9Challenge_Tables.sql
+++ Module9Challenge_Tables.sql
@@ -1,46 +1,53 @@
--- Departments table
-CREATE TABLE IF NOT EXISTS public.departments
+-- Titles table
+CREATE TABLE IF NOT EXISTS public.titles
 (
-    dept_no character varying(20) COLLATE pg_catalog."default" PRIMARY KEY,
-    dept_name character varying(20) COLLATE pg_catalog."default"
-);
-
--- Department Employees table
-CREATE TABLE IF NOT EXISTS public.dept_emp
-(
-    emp_no integer PRIMARY KEY,
-    dept_no character varying(10) COLLATE pg_catalog."default"
-);
-
--- Department Managers table
-CREATE TABLE IF NOT EXISTS public.dept_manager
-(
-    dept_no character varying(10) COLLATE pg_catalog."default" PRIMARY KEY,
-    emp_no integer
+    title_id VARCHAR(20) PRIMARY KEY,
+    title VARCHAR(20) NOT NULL
 );
 
 -- Employees table
 CREATE TABLE IF NOT EXISTS public.employees
 (
-    emp_no integer PRIMARY KEY,
-    emp_title_id character varying(20) COLLATE pg_catalog."default",
-    birth_date date,
-    first_name character varying(20) COLLATE pg_catalog."default",
-    last_name character varying(20) COLLATE pg_catalog."default",
-    sex character varying(20) COLLATE pg_catalog."default",
-    hire_date date
+    emp_no INTEGER PRIMARY KEY,
+    emp_title_id VARCHAR(20) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    sex VARCHAR(20) NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
 
 -- Salaries table
 CREATE TABLE IF NOT EXISTS public.salaries
 (
-    emp_no integer PRIMARY KEY,
-    salary integer
+    emp_no INTEGER PRIMARY KEY,
+    salary INTEGER NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
--- Titles table
-CREATE TABLE IF NOT EXISTS public.titles
+-- Departments table
+CREATE TABLE IF NOT EXISTS public.departments
 (
-    title_id character varying(20) COLLATE pg_catalog."default" PRIMARY KEY,
-    title character varying(20) COLLATE pg_catalog."default"
+    dept_no VARCHAR(20) PRIMARY KEY,
+    dept_name VARCHAR(20) NOT NULL
+);
+
+-- Department Employees table
+CREATE TABLE IF NOT EXISTS public.dept_emp
+(
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR(20) NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Department Managers table
+CREATE TABLE IF NOT EXISTS public.dept_manager
+(
+    dept_no VARCHAR(20) PRIMARY KEY,
+    emp_no INTEGER NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
